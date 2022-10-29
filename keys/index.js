88d57c41b98ea29f1f67ad5802ae8cf39b744dd5
Vyacheslav <ResSlavkaRes@mail.ru>
2022-10-29 17:13:10 +0300

    add protection of keys v3

--- keys/index.js
+++ keys/index.js
@@ -4,7 +4,7 @@ export default await new Promise(async (resolve, reject) => {
             console.log("Выбраны ключи для продакшена")
             resolve(keys.default)
         } else {
-            const keys = await import('./keys.dev.js')
+            const keys = await import('./keysdev/keys.dev.js')
             console.log("Выбраны ключи для разработки: ", keys.default)
             resolve(keys.default)
         }
